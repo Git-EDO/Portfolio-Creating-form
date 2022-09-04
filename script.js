00e07dd435de99e895d3d668f9a7e1a462cf3362
Git-EDO <DemonEX@bk.ru>
2022-09-04 13:12:28 +0300

    commit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ tabs.forEach(tab => tab.addEventListener('click', function(e){
   const tabsBody = document.querySelectorAll('.tabs')
   tabsBody.forEach(tabsBody => tabsBody.classList.remove('active'))
   currentTab.classList.add('active')
-}))
+}));
 
 let configTextarea = document.querySelector('.config-textarea')
 configTextarea.value = `{
@@ -18,63 +18,57 @@ configTextarea.value = `{
   "inputs": [
       {
         "type": "number",
-        "label": "Number input",
-        "count": 6
+        "label": "Number input"
       },
       {
         "type": "string",
-        "label": "String input name",
-        "count": 3
+        "label": "String input name"
       },
       {
         "type": "textarea",
-        "label": "Textarea name",
-        "count": 1
+        "label": "Textarea name"
       },
       {
         "type": "checkbox",
         "label": "Checkbox name",
-        "count": 12
+        "count": 4
+      },
+      {
+        "type": "checkbox",
+        "label": "Checkbox name",
+        "count": 2
       },
       {
         "type": "date",
-        "label": "Date name",
-        "count": 2
+        "label": "Date name"
       },
       {
         "type": "radio",
         "label": "Radio name",
-        "count": 6
+        "count": 2
       }
   ]
-}`
+}`;
 
-let myJsonObj
+let myJsonObj;
 
 document.querySelector('.config-btn').addEventListener('click', function(){
   myJsonObj = JSON.parse(configTextarea.value)
 
-  let myArray = myJsonObj.inputs;
-  let myStringObject = myArray.find(item => item.type == 'string')
-  let myNumberObject = myArray.find(item => item.type == 'number')
-  let myTextareaObject = myArray.find(item => item.type == 'textarea')
-  let myCheckboxObject = myArray.find(item => item.type == 'checkbox')
-  let myDateObject = myArray.find(item => item.type == 'date')
-  let myRadioObject = myArray.find(item => item.type == 'radio')
+  let myArray = myJsonObj.inputs
   let formName = myJsonObj.formname
 
 const newFormName = (formName) => {
   const createFormName = document.createElement('h1')
   createFormName.textContent = formName
   document.querySelector('.result-body').appendChild(createFormName)
-}
+};
 
-const newCountInputs = () => {
-  if (myNumberObject.count > 0) {
+const newCountInputs = (item) => {
     const createCountInputs = document.createElement('div')
     createCountInputs.classList.add('input-container')
     let countTitle = document.createElement('h2')
-    countTitle.textContent = myNumberObject.label
+    countTitle.textContent = item.label
   
     document.querySelector('.result-body').appendChild(createCountInputs)
     createCountInputs.appendChild(countTitle)
@@ -83,62 +77,53 @@ const newCountInputs = () => {
     countInputsWrapper.classList.add('count-inputs-wrapper')
     createCountInputs.appendChild(countInputsWrapper)
 
-      for (let i = 0; i < myNumberObject.count; i++) {
-        const newCounter = document.createElement('div')
-        newCounter.classList.add('counter')
-        const newCount = document.createElement('input')
-        let someValue = 0
+    const newCounter = document.createElement('div')
+    newCounter.classList.add('counter')
+    const newCount = document.createElement('input')
+    let someValue = 0
+    newCount.value = someValue
+    newCount.classList.add('count-input')
+    const newCountUp = document.createElement('div')
+    newCountUp.classList.add('countup')
+    newCountUp.addEventListener('click', function(){
+      someValue++
+      newCount.value = someValue
+    })
+    const newCountDown = document.createElement('div')
+    newCountDown.classList.add('countdown')
+    newCountDown.addEventListener('click', function(){
+        someValue--
         newCount.value = someValue
-        newCount.classList.add('count-input')
-        // newCount.setAttribute('disabled', true)
-        const newCountUp = document.createElement('div')
-        newCountUp.classList.add('countup')
-        newCountUp.addEventListener('click', function(){
-          someValue++
-          newCount.value = someValue
-        })
-        const newCountDown = document.createElement('div')
-        newCountDown.classList.add('countdown')
-        newCountDown.addEventListener('click', function(){
-          someValue--
-          newCount.value = someValue
-        })
-        countInputsWrapper.appendChild(newCounter)
-        newCounter.appendChild(newCount)
-        newCounter.appendChild(newCountUp)
-        newCounter.appendChild(newCountDown)
-      }
-  }
-}
+      })
+    countInputsWrapper.appendChild(newCounter)
+    newCounter.appendChild(newCount)
+    newCounter.appendChild(newCountUp)
+    newCounter.appendChild(newCountDown)
+};
 
-const newStringInputs = () => {
-  if (myStringObject.count > 0) {
-    const createStringInputs = document.createElement('div')
-    createStringInputs.classList.add('input-container')
-    let stringTitle = document.createElement('h2')
-    stringTitle.textContent = myStringObject.label
+const newStringInputs = (item) => {
+      const createStringInputs = document.createElement('div')
+      createStringInputs.classList.add('input-container')
+      let stringTitle = document.createElement('h2')
+      stringTitle.textContent = item.label
+    
+      document.querySelector('.result-body').appendChild(createStringInputs)
+      createStringInputs.appendChild(stringTitle)
   
-    document.querySelector('.result-body').appendChild(createStringInputs)
-    createStringInputs.appendChild(stringTitle)
+      const stringInputsWrapper = document.createElement('div')
+      stringInputsWrapper.classList.add('string-inputs-wrapper')
+      createStringInputs.appendChild(stringInputsWrapper)
+  
+      const newString = document.createElement('input')
+      newString.classList.add('string-input')
+      stringInputsWrapper.appendChild(newString)
+};
 
-    const stringInputsWrapper = document.createElement('div')
-    stringInputsWrapper.classList.add('string-inputs-wrapper')
-    createStringInputs.appendChild(stringInputsWrapper)
-
-      for (let i = 0; i < myStringObject.count; i++) {
-        const newString = document.createElement('input')
-        newString.classList.add('string-input')
-        stringInputsWrapper.appendChild(newString)
-      }
-  }
-}
-
-const newTextareaInputs = () => {
-  if (myTextareaObject.count > 0) {
+const newTextareaInputs = (item) => {
     const createTextareaInputs = document.createElement('div')
     createTextareaInputs.classList.add('input-container')
     let textareaTitle = document.createElement('h2')
-    textareaTitle.textContent = myTextareaObject.label
+    textareaTitle.textContent = item.label
   
     document.querySelector('.result-body').appendChild(createTextareaInputs)
     createTextareaInputs.appendChild(textareaTitle)
@@ -147,20 +132,16 @@ const newTextareaInputs = () => {
     textareaInputsWrapper.classList.add('textarea-inputs-wrapper')
     createTextareaInputs.appendChild(textareaInputsWrapper)
 
-      for (let i = 0; i < myTextareaObject.count; i++) {
-        const newTextarea = document.createElement('textarea')
-        newTextarea.classList.add('textarea-input')
-        textareaInputsWrapper.appendChild(newTextarea)
-      }
-  }
-}
+    const newTextarea = document.createElement('textarea')
+    newTextarea.classList.add('textarea-input')
+    textareaInputsWrapper.appendChild(newTextarea)
+};
 
-const newCheckboxes = () => {
-  if (myCheckboxObject.count > 0) {
+const newCheckboxes = (item) => {
     const createCheckboxes = document.createElement('div')
     createCheckboxes.classList.add('input-container')
     let checkboxesTitle = document.createElement('h2')
-    checkboxesTitle.textContent = myCheckboxObject.label
+    checkboxesTitle.textContent = item.label
   
     document.querySelector('.result-body').appendChild(createCheckboxes)
     createCheckboxes.appendChild(checkboxesTitle)
@@ -169,12 +150,13 @@ const newCheckboxes = () => {
     checkboxesInputsWrapper.classList.add('checkboxes-inputs-wrapper')
     createCheckboxes.appendChild(checkboxesInputsWrapper)
 
-      for (let i = 0; i < myCheckboxObject.count; i++) {
+
+      for (let i = 0; i < item.count; i++) {
         const newCheckbox = document.createElement('input')
         newCheckbox.type = 'checkbox'
+
         newCheckbox.id = 'checkbox' + (i+1)
         const newCheckboxLabel = document.createElement('label')
-        newCheckboxLabel.setAttribute('for', `checkbox${i+1}`)
         newCheckboxLabel.textContent = 'Label for ' + (i+1)
 
         const checkboxFlex = document.createElement('div')
@@ -184,15 +166,13 @@ const newCheckboxes = () => {
         checkboxFlex.appendChild(newCheckbox)
         checkboxFlex.appendChild(newCheckboxLabel)
       }
-  }
-}
+};
 
-const newDateElement = () => {
-  if (myDateObject.count > 0) {
+const newDateElement = (item) => {
     const createDateSection = document.createElement('div')
     createDateSection.classList.add('input-container')
     let dateTitle = document.createElement('h2')
-    dateTitle.textContent = myDateObject.label
+    dateTitle.textContent = item.label
   
     document.querySelector('.result-body').appendChild(createDateSection)
     createDateSection.appendChild(dateTitle)
@@ -201,24 +181,19 @@ const newDateElement = () => {
     dateInputsWrapper.classList.add('date-inputs-wrapper')
     createDateSection.appendChild(dateInputsWrapper)
 
-      for (let i = 0; i < myDateObject.count; i++) {
-        let createDate = new Date();
-        let createdDate = `${createDate.getDate()}.${createDate.getMonth()}.${createDate.getFullYear()}`;
-        const newDate = document.createElement('input')
-        newDate.setAttribute('disabled', true)
-        newDate.classList.add('date-input')
-        newDate.value = createdDate
-        dateInputsWrapper.appendChild(newDate)
-      }
-  }
-}
+    let createDate = new Date();
+    let createdDate = `${createDate.getDate()}.${createDate.getMonth()}.${createDate.getFullYear()}`;
+    const newDate = document.createElement('input')
+    newDate.classList.add('date-input')
+    newDate.value = createdDate
+    dateInputsWrapper.appendChild(newDate)
+};
 
-const newRadios = () => {
-  if (myRadioObject.count > 0) {
+const newRadios = (item) => {
     const createRadioSection = document.createElement('div')
     createRadioSection.classList.add('input-container')
     let radioTitle = document.createElement('h2')
-    radioTitle.textContent = myRadioObject.label
+    radioTitle.textContent = item.label
   
     document.querySelector('.result-body').appendChild(createRadioSection)
     createRadioSection.appendChild(radioTitle)
@@ -227,13 +202,12 @@ const newRadios = () => {
     radioInputsWrapper.classList.add('radio-inputs-wrapper')
     createRadioSection.appendChild(radioInputsWrapper)
 
-      for (let i = 0; i < myRadioObject.count; i++) {
+      for (let i = 0; i < item.count; i++) {
         const newRadio = document.createElement('input')
         newRadio.type = 'radio'
         newRadio.name = 'radio'
         newRadio.id = 'radio' + (i+1)
         const newRadioLabel = document.createElement('label')
-        newRadioLabel.setAttribute('for', `radio${i+1}`)
         newRadioLabel.textContent = 'Label for ' + (i+1)
 
         const radioFlex = document.createElement('div')
@@ -243,21 +217,44 @@ const newRadios = () => {
         radioFlex.appendChild(newRadio)
         radioFlex.appendChild(newRadioLabel)
       }
-  }
-}
+};
 
 function createForm() {
-  newFormName(formName)
-  newCountInputs()
-  newStringInputs()
-  newTextareaInputs()
-  newCheckboxes()
-  newDateElement()
-  newRadios()
-}
+  document.querySelector('.result-body').innerHTML = ""
 
-createForm()
-})
+  newFormName(formName)
+
+  myArray.forEach(item => {
+    if (item.type == 'number') {
+      newCountInputs(item)
+    } else
+    if (item.type == 'string') {
+      newStringInputs(item)
+    } else
+    if (item.type == 'textarea') {
+      newTextareaInputs(item)
+    } else
+    if (item.type == 'checkbox') {
+      newCheckboxes(item)
+    } else
+    if (item.type == 'date') {
+      newDateElement(item)
+    } else 
+    if (item.type == 'radio') {
+      newRadios(item)
+    }
+  })
+};
+
+createForm();
+
+});
+
+document.querySelector('.reset-btn').addEventListener('click', function(){
+    document.querySelector('.result-body').innerHTML = ""
+});
+
+
 
 
 
